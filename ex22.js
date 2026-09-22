265e9a4f99c8e8ece12faef75c9a8f96fb6d740c
salma-ait-bouras <salmaaitbouras@gmail.com>
2026-09-22 09:18:35 +0100

    Corriger une variable globale accidentelle

---
+++ ex22.js
@@ -0,0 +1,8 @@
+let x = "externe";
+{
+    let x = "milieu";
+    {
+        console.log(x);
+    }
+}
+        console.log(x);
---
+++ ex23.js
@@ -0,0 +1,6 @@
+function setScore(){ 
+    score = 100; 
+    return score ;
+}
+console.log(setScore())
+// la faute c'est que dans le premier code la fonction ne return pas la valeur du variable "score"
