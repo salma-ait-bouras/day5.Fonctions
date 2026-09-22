7aba1426aac76aa3ebc54149c3072c74a937b8d3
salma-ait-bouras <salmaaitbouras@gmail.com>
2026-09-22 10:58:40 +0100

    Compteur avec portée privée

---
+++ ex24.js
@@ -0,0 +1,8 @@
+let cout=0
+const counter = () => cout +=1
+function  makeCounter (){
+    return counter()
+    };
+console.log(makeCounter()); 
+console.log(makeCounter()); 
+console.log(makeCounter()); 
